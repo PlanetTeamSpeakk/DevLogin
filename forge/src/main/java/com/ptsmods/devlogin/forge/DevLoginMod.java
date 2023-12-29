25f83546f1ef8e3030ded53796dc5524a393d2b5
PlanetTeamSpeak <PlanetTeamSpeakk@users.noreply.github.com>
2023-12-29 19:25:10 +0100

    Add NeoForge support Not entirely certain it works, but it should.

--- forge/src/main/java/com/ptsmods/devlogin/forge/DevLoginMod.java
+++ forge/src/main/java/com/ptsmods/devlogin/forge/DevLoginMod.java
@@ -2,6 +2,6 @@ package com.ptsmods.devlogin.forge;
 
 import net.minecraftforge.fml.common.Mod;
 
-// Just here so Forge know that this is in fact a mod.
+// Just here so Forge knows that this is in fact a mod.
 @Mod("devlogin")
 public class DevLoginMod {}
---
+++ neoforge/src/main/java/com/ptsmods/devlogin/neoforge/DevLoginMod.java
@@ -0,0 +1,7 @@
+package com.ptsmods.devlogin.neoforge;
+
+import net.neoforged.fml.common.Mod;
+
+// Just here so NeoForge knows that this is in fact a mod.
+@Mod("devlogin")
+public class DevLoginMod {}
